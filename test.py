4bdb996fb2a3304572e5448d1932848aeddd82ea
ariakhademi <ariakhademi@gmail.com>
2024-10-17 16:51:10 -0400

    transformer article

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print('hello')
+nums = [1,1,2,4,4,4,4,5,5,5,6,7]
+
+visited = []
+for idx, val in enumerate(nums):
+    if val not in visited:
+        visited.append(val)
+    else:
+        del nums[idx]
+
+print(nums)
